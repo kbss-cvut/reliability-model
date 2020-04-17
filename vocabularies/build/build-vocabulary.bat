a1db9ded146950b0dfa71d2925a0792bad872755
Bogdan Kostov <kostobog@fel.cvut.cz>
2020-04-17 10:07:42 +0200

    minor changes in build script of aviation profile based on ATA 100 terms of the reliability ontology

--- vocabularies/build/build-vocabulary.bat
+++ vocabularies/build/build-vocabulary.bat
@@ -4,8 +4,14 @@ set ata100=%ataroot%ata-100.ttl
 set output=..\reliability-aviation-profile-components
 
 call jena-cmd.bat
+
 call arq --data=%ata100% --query=ata-100-a-components.sparql > ata-100-components.ttl
-call arq --data=%ata100% --query=ata-100-a-has-component.sparql > ata-100-has-component.ttl
-call riot --output=TTL --formatted=TTL reliability-aviation-profile-components-header.ttl ata-100-components.ttl ata-100-has-component.ttl > %output%.ttl
+REM call arq --data=%ata100% --query=ata-100-a-has-component.sparql > ata-100-has-component.ttl
+
+call riot --output=TTL --formatted=TTL ^
+reliability-aviation-profile-components-header.ttl ^
+ata-100-components.ttl > %output%.ttl
+
 call riot --output=RDFXML %output%.ttl > %output%.rdf
+
 @echo on
